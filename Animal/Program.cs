546c16d874cbb5fa083386f1435dd28fc7db440f
BetrandAntolim <antontheamor123@gmail.com>
2019-05-14 14:50:20 +0700

    Animal

---
+++ Animal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Name = "Animal";
+            animal.Speak();
+
+            Dog dog = new Dog();
+            dog.Name = "Spike";
+            animal = dog;
+            dog.Speak();
+
+            Cat cat = new Cat();
+            cat.Name = "Tom";
+            animal = cat;
+            cat.Speak();
+
+            Console.ReadKey();
+        }
+    }
+}
